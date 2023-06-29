79d20844b51c4e8d2d9eee8a75352e3af15e5f51
lolo859 <80057375+lolo859@users.noreply.github.com>
2023-06-29 12:19:03 +0200

    Delete module directory

--- module/uuid.py
+++
@@ -1,9 +0,0 @@
-import uuid
-import random
-def execute():
-    uuid.uuid4()
-    while True:
-        id=str(uuid.uuid4())
-        trimmed=id[:random.randint(0,len(id)-1)]
-        spaces=" " * random.randint(0,15)
-        print(f"{spaces}{trimmed}")
